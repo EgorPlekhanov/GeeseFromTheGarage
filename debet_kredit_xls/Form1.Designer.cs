c8a3f6f271b08f4387da9fdb796906ac3aafffa1
eg0r.plekhan0v@hotmail.com <eg0r.plekhan0v@hotmail.com>
2021-03-16 14:26:04 +0500

    Add project files.

---
+++ debet_kredit_xls/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace debet_kredit_xls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.добавитьДанныеИзExcelToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очитьДанныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьОтсортированныеДанныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 47);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(482, 508);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьДанныеИзExcelToolStripMenuItem,
+            this.очитьДанныеToolStripMenuItem,
+            this.сохранитьОтсортированныеДанныеToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(290, 70);
+            // 
+            // добавитьДанныеИзExcelToolStripMenuItem
+            // 
+            this.добавитьДанныеИзExcelToolStripMenuItem.Name = "добавитьДанныеИзExcelToolStripMenuItem";
+            this.добавитьДанныеИзExcelToolStripMenuItem.Size = new System.Drawing.Size(289, 22);
+            this.добавитьДанныеИзExcelToolStripMenuItem.Text = "Добавить данные из excel...";
+            this.добавитьДанныеИзExcelToolStripMenuItem.Click += new System.EventHandler(this.добавитьДанныеИзExcelToolStripMenuItem_Click);
+            // 
+            // очитьДанныеToolStripMenuItem
+            // 
+            this.очитьДанныеToolStripMenuItem.Name = "очитьДанныеToolStripMenuItem";
+            this.очитьДанныеToolStripMenuItem.Size = new System.Drawing.Size(289, 22);
+            this.очитьДанныеToolStripMenuItem.Text = "Очить данные";
+            this.очитьДанныеToolStripMenuItem.Click += new System.EventHandler(this.очитьДанныеToolStripMenuItem_Click);
+            // 
+            // сохранитьОтсортированныеДанныеToolStripMenuItem
+            // 
+            this.сохранитьОтсортированныеДанныеToolStripMenuItem.Name = "сохранитьОтсортированныеДанныеToolStripMenuItem";
+            this.сохранитьОтсортированныеДанныеToolStripMenuItem.Size = new System.Drawing.Size(289, 22);
+            this.сохранитьОтсортированныеДанныеToolStripMenuItem.Text = "Сохранить отфильтрованные данные...";
+            this.сохранитьОтсортированныеДанныеToolStripMenuItem.Click += new System.EventHandler(this.сохранитьОтсортированныеДанныеToolStripMenuItem_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Все",
+            "Должники",
+            "Без долга"});
+            this.comboBox1.Location = new System.Drawing.Point(68, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Фильтр;";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 557);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Дебет_Кредит v.10";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem добавитьДанныеИзExcelToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очитьДанныеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьОтсортированныеДанныеToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ debet_kredit_xls/Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace debet_kredit_xls
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            dt.Columns.Add("Статус клиента", Type.GetType("System.String"));
+            dt.Columns.Add("№ договора", Type.GetType("System.String"));
+            dt.Columns.Add("Баланс", Type.GetType("System.Double"));
+
+        }
+
+        private void добавитьДанныеИзExcelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            o.Filter = "excel files (*.xls)|*.xls|All files (*.*)|*.*";
+            o.RestoreDirectory = true;
+            string file = string.Empty;
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+                file = o.FileName;
+                if (File.Exists(file))
+                {
+                    LoadDataFromFile(file);
+                }
+            }
+        }
+
+        private void LoadDataFromFile(string file)
+        {
+            if (!backgroundWorker1.IsBusy)
+            {
+                backgroundWorker1.RunWorkerAsync(file);
+            }
+
+            
+        }
+
+
+        private void очитьДанныеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dt.Rows.Clear();
+            dataGridView1.Rows.Clear();
+        }
+
+        private void сохранитьОтсортированныеДанныеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            s.RestoreDirectory = true;
+            string file = string.Empty;
+            if (s.ShowDialog() == DialogResult.OK)
+            {
+                SaveDataToFile(s.FileName);
+                MessageBox.Show("Данные сохранены");
+            }
+        }
+
+        private void SaveDataToFile(string file)
+        {
+            string scv_data = string.Empty;
+            //for (int f = 0; f < dataGridView1.ColumnCount; f++)
+            //{
+            //    scv_data += (dataGridView1.Columns[f].HeaderText + ";");
+
+            //}
+            //scv_data += "\t\n";
+            for (int i = 0; i < dataGridView1.RowCount - 1; i++)
+            {
+                int flag = 0;
+                if (dataGridView1.Rows[i].Cells[0].Value.ToString() == "Информирование")
+                {
+                    flag = 1;
+                }
+               scv_data += string.Format("{0};{0};{1};1000;{2};{3}",dataGridView1.Rows[i].Cells[1].Value.ToString(), dataGridView1.Rows[i].Cells[2].Value.ToString(), DateTime.Now.ToString("dd-MM-yyyy"),flag);
+               scv_data += "\t\n";
+            }
+            StreamWriter wr = new StreamWriter(file, false, Encoding.GetEncoding("windows-1251"));
+            wr.Write(scv_data);
+            wr.Close();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            string file = (string)e.Argument;
+            try
+            {
+                Microsoft.Office.Interop.Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application(); //Excel
+                Microsoft.Office.Interop.Excel.Workbook xlWB; //рабочая книга              
+                Microsoft.Office.Interop.Excel.Worksheet xlSht; //лист Excel   
+                xlWB = xlApp.Workbooks.Open(file); //название файла Excel                                             
+                xlSht = xlWB.Worksheets["Sheet1"]; //название листа или 1-й лист в книге xlSht = xlWB.Worksheets[1];
+                int iLastRow = xlSht.Cells[xlSht.Rows.Count, "A"].End[Microsoft.Office.Interop.Excel.XlDirection.xlUp].Row;  //последняя заполненная строка в столбце А            
+                var arrData = (object[,])xlSht.Range["A3:G" + iLastRow].Value; //берём данные с листа Excel
+                                                                               //xlApp.Visible = true; //отображаем Excel     
+                xlWB.Close(false); //закрываем книгу, изменения не сохраняем
+                xlApp.Quit(); //закрываем Excel
+
+                e.Result = arrData;
+            }
+            catch (SystemException ex)
+            {
+                e.Result = null;
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error == null && e.Result != null)
+            {
+                var arrData = (object[,])e.Result;
+                dt.Rows.Clear();
+                int RowsCount = arrData.GetUpperBound(0);
+                int ColumnsCount = arrData.GetUpperBound(1);
+                //заполняем DataGridView данными из массива
+                for (int i = 1; i <= RowsCount; i++)
+                {
+                    DataRow dr = dt.NewRow();
+                    dr[1] = arrData[i, 1];
+                    double balans = 0;
+                    if (arrData[i, 6] != null)
+                    {
+                        try
+                        {
+                            balans = Convert.ToDouble(arrData[i, 6]);
+
+                        }
+                        catch (SystemException ex)
+                        {
+                            dr[2] = -1;
+                            dr[0] = "ошибка исходных данных";
+                            dt.Rows.Add(dr);
+                            continue;
+                        }
+                    }
+                    else if (arrData[i, 7] != null)
+                    {
+                        try
+                        {
+                            balans = -Convert.ToDouble(arrData[i, 7]);
+                        }
+                        catch (SystemException ex)
+                        {
+                            balans = -1;
+                            dr[0] = "ошибка исходных данных";
+                            dt.Rows.Add(dr);
+                            continue;
+                        }
+                    }
+
+                    dr[2] = balans;
+
+                    if (balans < 0)
+                    {
+                        dr[0] = "Информирование";
+                    }
+                    else
+                    {
+                        dr[0] = "Нет задолженности";
+                    }
+
+                    dt.Rows.Add(dr);
+
+
+                }
+
+                dataGridView1.DataSource = dt;
+                FilterData();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FilterData();
+
+        }
+
+        private void FilterData()
+        {
+            if (comboBox1.Text == "Все")
+            {
+                dataGridView1.DataSource = dt;
+            }
+            else if (comboBox1.Text == "Должники")
+            {
+                DataTable dt1 = dt.Copy();
+                for (int i = 0; i < dt1.Rows.Count; i++)
+                {
+                    if (dt1.Rows[i]["Статус клиента"].ToString() != "Информирование")
+                    {
+                        dt1.Rows.RemoveAt(i);
+                        i--;
+                        continue;
+                    }
+                }
+
+                dataGridView1.DataSource = dt1;
+                //DataRow[] rows = dt.Select("[Статус клиента] = 'Информирование'");
+
+            }
+            else if (comboBox1.Text == "Без долга")
+            {
+                //DataRow[] rows = dt.Select("[Статус клиента] = 'Нет задолженности'");
+                DataTable dt1 = dt.Copy();
+                for (int i = 0; i < dt1.Rows.Count; i++)
+                {
+                    if (dt1.Rows[i]["Статус клиента"].ToString() != "Нет задолженности")
+                    {
+                        dt1.Rows.RemoveAt(i);
+                        i--;
+                        continue;
+                    }
+                }
+
+                dataGridView1.DataSource = dt1;
+            }
+        }
+    }
+}
